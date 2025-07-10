ff43434362a8aeb953e208a66fb3de5c91158294
Asun Gomez Castro <agcastro7@gmail.com>
2025-07-10 11:54:28 +0200

    Add new test

--- e2e-tests/authentication/test_unauthenticated.py
+++ e2e-tests/authentication/test_unauthenticated.py
@@ -1,15 +1,27 @@
-from utils import Helper
 from playwright.sync_api import Page, expect
 
 
 def test_visit_page_without_authentication(
-    tests_helper: Helper,
     page: Page
 ) -> None:
     """
     Test that visiting the page without stored credentials shows the
     login page.
     """
+    page.goto("/")
+    expect(
+        page.get_by_text("Log in to this wonderfully useful site")
+    ).to_be_visible()
+
+
+def test_visit_page_with_expired_credentials(
+    page: Page
+) -> None:
+    """
+    Test that visiting the page with expired credentials shows the
+    login page with a message indicating the session has expired.
+    """
+
     page.goto("/")
     expect(
         page.get_by_text("Log in to this wonderfully useful site")
