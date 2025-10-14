86878abab7f2844ccdd3c831f29bed7c9aadc7d7
Asun Gomez Castro <agcastro7@gmail.com>
2025-10-14 15:47:20 +0200

    add e2e test for mocking users

--- e2e-tests/src/authentication/test_switch_user.py
+++ e2e-tests/src/authentication/test_switch_user.py
@@ -20,3 +20,35 @@ def test_switch_user_as_non_admin(
         page.goto("/")
         page.get_by_label("User menu").click()
         expect(page.get_by_text("Switch user")).not_to_be_visible()
+
+
+def test_switch_user_when_authenticated_as_admin(
+    page: Page,
+    tests_helper: Helper,
+    user_factory: UserFactory
+) -> None:
+    """
+    Test that admins can switch users
+    """
+    admin_user = user_factory.generate(
+        is_superuser=True,
+        email="admin.email@email.net"
+    )
+    tests_helper.insert_user(admin_user)
+    impersonated_user = user_factory.generate(
+        email="impersonated.email@email.net"
+    )
+    tests_helper.insert_user(impersonated_user)
+    with tests_helper.authenticated_context(
+        page=page,
+        email=admin_user.email
+    ):
+        page.goto("/")
+        page.get_by_label("User menu").click()
+        expect(page.get_by_text("Switch user")).to_be_visible()
+        page.get_by_placeholder("Type an email...").fill(
+            impersonated_user.email
+        )
+        page.get_by_text(impersonated_user.email).click()
+        expect(page.get_by_text(impersonated_user.email)).to_be_visible()
+        expect(page.get_by_text(admin_user.email)).not_to_be_visible()
